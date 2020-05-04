54dbab87736db4379332c5e3fa9ee41e459a2222
Ishita Date <ishitadate@gmail.com>
2020-05-04 17:04:08 -0400

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 print("Welcome to our NSF Class. My name is ", end = "")
 aList = ["i", "s", "h", "i", "t", "a", " ", "!"]
 
-for i in range(len(aList))
+for i in range(len(aList)):
 	print(aList[i], end = "")
 
 print("See you on thursday!")
