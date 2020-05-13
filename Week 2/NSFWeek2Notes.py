59fda4846fdc070bc534aa815e475f7da3b444aa
Ishita Date <ishitadate@gmail.com>
2020-05-13 14:13:38 -0400

    Update NSFWeek2Notes.py
    Update w class outline

--- Week 2/NSFWeek2Notes.py
+++ Week 2/NSFWeek2Notes.py
@@ -1,2 +1,39 @@
-NSF Week 2 Notes
+# NSF Week 2 Notes
 
+# Review:
+# - Commenting (highlight cells)
+#   - Mac: "cmd" + "/"
+#   - Windows: "ctrl" + "k" + "ctrl" + "c"
+#   - repl: "f1" + type "toggle line comment"
+
+
+# Operations:
+# - Numbers
+#   - Add
+#   - Subtract
+#   - Multiplication
+#   - Division
+#   - Integer division
+#   - Modulus
+
+# - Strings
+#   - + and *
+#   - - and /
+
+# - Combining Operators with Assignment
+
+# - Comparison Operators
+
+
+# If/Else:
+# - Basic Structure
+
+# - Elif
+
+# - Keywords
+#   - pass
+#   - break
+
+# Input from User:
+# - Built in python methods
+#   - https://docs.python.org/3/library/functions.html
