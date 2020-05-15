dfbd755526a5baaf90eabf9c90b011bad11bf884
Ishita Date <ishitadate@gmail.com>
2020-05-15 09:44:56 -0400

    Create w2_homework.py

---
+++ Week 2/w2_homework.py
@@ -0,0 +1,29 @@
+"""
+Guesser Game Part I:
+In this game the user wants to guess your string.
+
+Pick a value for a string.
+
+Take an input from the user, which is their guess.
+
+If the guess is correct, print "good job!"
+If the guess is wrong print "try again"
+"""
+
+
+
+""" 
+Guesser Game Part II:
+In this game the user is trying to guess your number
+
+Pick a value for an inital number.
+
+Take an input from the user. 
+
+If the input is less than your number, print "guess higher"
+If the input is greater than your number, print "guess lower"
+If the input is correct print "good job"
+
+Hint: The input function is python will read th number as a string. 
+You will have to cast it to an int so the condition statements work. (see line 144 in the notes)
+"""
