975dade3589947636538d7b018d2be3fb4f74405
Ishita Date <ishitadate@gmail.com>
2020-05-21 13:11:19 -0400

    Create NSFWeek3Notes.py

---
+++ Week 3/NSFWeek3Notes.py
@@ -0,0 +1,35 @@
+NSFWeek3Notes.py
+
+repl.it review
+- make new repl and use the "run" button
+	- backend code to interpret as a python file
+	- similar to an IDE
+- use the class links for the github repo
+
+homework review/questions
+
+nested if statements
+
+-----
+
+Loops: repeat lines of code
+
+Logic
+
+Types of Loops (basic)
+while loop:
+
+else along with while... another way to write what is above
+
+
+for loop: 
+
+range function
+
+---
+
+Nested loops:
+
+---
+
+Loop control statements
