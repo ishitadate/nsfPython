88251efe082eb851cb3002a493155d6a9df75be3
Ishita Date <ishitadate@gmail.com>
2020-05-22 21:09:46 -0400

    Create w3_homework.py

---
+++ Week 3/w3_homework.py
@@ -0,0 +1,23 @@
+""" 
+Number One:
+
+Using a for loop, output how many odd and even integers are in the following list.
+
+Expected Output :
+Number of even numbers : 5
+Number of odd numbers : 4
+"""
+print("Number 1")
+numbersOne = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+
+
+""" 
+Number Two:
+
+Using a for loop, find the maximum of the following list
+
+Expected Output :
+Maximum : 10
+"""
+print("Number 2")
+numbersTwo = (1, -5, 3, 4, 10, 8, -9) 
