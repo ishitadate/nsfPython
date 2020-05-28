0c54e922135f78a2c3a9ae542f01085a5cc16fb5
Ishita Date <ishitadate@gmail.com>
2020-05-28 15:13:17 -0400

    upload wk4 notes template

---
+++ Week 4/NSFWeek4Notes.py
@@ -0,0 +1,110 @@
+# NSFWeek4Notes.py
+
+# repl.it issues will be addressed at the end of class
+
+# Homework 3 Review
+
+""" 
+Number One:
+Using a for loop, output how many odd and even integers are in the following list.
+Expected Output :
+Number of even numbers : 5
+Number of odd numbers : 4
+"""
+print("Number 1")
+numbersOne = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+
+
+""" 
+Number Two:
+Using a for loop, find the maximum of the following list
+Expected Output :
+Maximum : 10
+"""
+print("Number 2")
+numbersTwo = (1, -5, 3, 4, 10, 8, -9) 
+
+
+# ---------
+
+# Strings
+
+# ---------
+
+# String operations (review)
+
+
+# ---------
+
+# Accessing characters in a string
+
+# - Indexing
+
+# - Negative Indexing
+
+# - Range
+
+# Example
+
+aStr = "Hello World"
+
+# print the first char of the word using two differrent methods
+# print the first three chars of the word
+# print the last four chars of the word
+# print all but the first 5 chars
+
+# ---------
+
+# Testing Strings
+# Check out "https://docs.python.org/2.5/lib/string-methods.html" for more!
+
+# Example
+
+aStr1 = "summer is "
+aStr2 = "100%"
+aStr2 = " the best"
+
+"""
+We want to combine the strings, only if the format is what we want...
+1. String 1 is only alphabetic 
+2. String 2
+	a. The last char is %
+	b. The rest of the chars are numeric
+3. String 3 is uppercase
+
+Write nested if statements to check these conditions, if they are all true, print the concatanation.
+"""
+
+# ---------
+
+# Other string functions
+
+# - Strip
+
+# - Upper and Lower
+
+# ---------
+
+# Iterating through a string is a way that we can do - or /
+
+# Example- Remove all the l's in the following string
+
+oldString = "lilllltslatlllhurlsdlayl"
+
+# ---------
+
+# Practice Problems
+
+# 1. Swap the letters in the following string
+aStr1 = "The BEST kinD of coOKies are sugar. NO CAP."
+
+# 2. Find how many instances of "abc" are in the following string
+aStr2 = "abcabbcabbcabbcabcaabcabcadbcacbchabc"
+
+# 3. Remove any subsequent occurrence of non-distinct characters in the following string. For example: aab becomes ab
+aStr3 = "aaabbbcbbcaabbca"
+
+
+
+
+
