6379e579f824ebd3b84ad5bd2de2213834008056
Ishita Date <ishitadate@gmail.com>
2020-05-29 10:42:25 -0400

    Create w4_homework.py

---
+++ Week 4/w4_homework.py
@@ -0,0 +1,16 @@
+""" 
+Section One: Indexing
+
+You will use the following aString for all parts of this section.
+"""
+
+aString = "I like to cook!"
+
+# Number One: Print the first 3 chars of the string 
+
+# Number Two: Print the last 3 chars of the string using 2 methods
+
+# Number Three: Print everything but the first 5 chars of the string using 2 methods
+
+# Number Four: Print everything but the last 2 chars of the string using 2 methods
+
