344ce9d3e4c85f12fd229811ac0909dd1d3bc998
Ishita Date <ishitadate@gmail.com>
2020-05-29 10:47:50 -0400

    Update w4_homework.py

--- Week 4/w4_homework.py
+++ Week 4/w4_homework.py
@@ -12,5 +12,41 @@ aString = "I like to cook!"
 
 # Number Three: Print everything but the first 5 chars of the string using 2 methods
 
-# Number Four: Print everything but the last 2 chars of the string using 2 methods
+
+
+""" 
+Section Two: Writing code
+"""
+
+"""
+Number One:
+
+I have three strings, but I only want to combine the strings if the format is what I want...
+
+1. String 1 is only alphabetic 
+  2. String 2
+ 	a. The first char is $
+ 	b. The rest of the chars are numeric
+3. String 3 is uppercase
+
+Write nested if statements to check these conditions, if they are all true, print the concatanation.
+Hint: If you get stuck, reference the notes for help.
+"""
+
+aStr1 = "MyPizzaCosts "
+aStr2 = "$700"
+aStr3 = " THATSALOT"
+
+"""
+Number Two: Remove all the l's in oldString
+
+Expected Output: 
+
+the newstring is: itsathursday
+Hint: You may want to use a for loop to iterate through the string
+"""
+
+oldString = "lillLltslatlllhurLsdlayL"
+
+
 
