925d31f915bc66038a3bebcb76ad3e79a70e4f0f
suhanib <38591664+suhanib@users.noreply.github.com>
2020-06-15 17:35:31 -0400

    w5 homework update

--- Week 5/w5_homework.py
+++ Week 5/w5_homework.py
@@ -1,34 +1,15 @@
-"""
+""" 
 Question 1
-If I give you two lists of numbers, determine a list of all the numbers they have in common.
-For example:
-one = [1,2,3]
-two = [2,3,4]
-result = [2,3]
+If I give you a list, write something that creates a new list that contains only the first list's unique elements, 
+no duplicates.
 """
+listOne = [1,2,4,3,1,1,1,5,6,3,3,2,6,4,6,7]
+# Expected output: newlist = [1,2,4,3,5,6,7] (order of numbers doesn't matter)
 
-listOne = [3,4,7,8,9]
-listTwo = [4,5,6,7]
-
-# Expected output:
-# "the result is [4,7]"
-
-"""
+""" 
 Question 2
-If I give you a list, determine if it is a palindrome or not.
+If I give you a list, write something that will remove all duplicates.
+DIFFERENCE BETWEEN Q1 and 2: you are changing the original list here, not creating a new one.
 """
-
-listThree = ['a','b','c',1,'c','b','a']
-# Expected output: this is a palindrome
-listFour = [1,2,3,'b','d', 3,2,1]
-# Expected output: this is not a palindrome
-
-"""
-Question 3
-If I give you a list, write something that takes this list and makes a new list that has only the even 
-elements of this list in it.
-BONUS: Write it in only 1 line.
-"""
-
-listFive = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-# Expected output: [4,16,36,64,100]
+listTwo = [1,2,4,3,1,1,1,5,6,3,3,2,6,4,6,7]
+# Expected output: listTwo = [1,2,4,3,5,6,7] (again, order doesn't matter)
