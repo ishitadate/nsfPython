19a4f16a75cdd8f6d91aa226d1988f2792f5afba
suhanib <38591664+suhanib@users.noreply.github.com>
2020-07-24 14:47:19 -0400

    w10 HW; PROJECT?
    hey all! this is some practice on our last topic, recursion. let me know if you guys want a fun project. i can put up some material on simple game development.

---
+++ Week 10/w10_homework.py
@@ -0,0 +1,8 @@
+# hello all! these are some practice problems for our last topic, recursion!
+# let me know if you guys want me to put up a project (quick and simple video game development, anyone?)
+
+# QUESTION 1: function recursiveSum(n); compute the sum of all numbers from 1 to n recursively
+
+# QUESTION 2: function recursiveDigits(n); compute how many digits a number has, recursively
+
+# QUESTION 3: function recursiveMax(listOfIntegers); find the max of the list recursively
